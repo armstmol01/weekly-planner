1917481bee05bea5ba25eb3c8439293e5ddbe973
armstmol01 <armstmol01@gmail.com>
2022-08-26 11:43:51 -0700

    test api

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const path = require('path');
+
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -6,7 +9,35 @@ const pool = new Pool({
   }
 });
 
+const app = express();
+
+// Put all API endpoints under '/api'
+const SERVER_ERROR_CODE = 500;
+const CLIENT_ERROR_CODE = 400;
+
 // Express only serves static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+// let db = await pool.connect();
+// await db.query(qry, [access_token, refresh_token, expires_at, 90470]);
+// db.release();
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
+
+app.get('/test', async (req, res) => {
+  try {
+    res.send("hey");
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port);

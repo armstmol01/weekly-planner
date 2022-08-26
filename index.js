c919fbff604ea9add28b097edbd32279967c22ff
armstmol01 <armstmol01@gmail.com>
2022-08-26 13:07:01 -0700

    database setup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+'use strict';
 const express = require('express');
 const path = require('path');
+const crypto = require('crypto');
 
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -39,6 +41,34 @@ app.get('/test', async (req, res) => {
     res.send("Error " + err);
   }
 })
+
+/**
+* generates random string of characters i.e salt
+* @function
+* @param {number} length - Length of the random string.
+*/
+function genSalt(length) {
+  return crypto.randomBytes(Math.ceil(length/2))
+  .toString('hex') /** convert to hexadecimal format */
+  .slice(0,length); /** return required number of characters */
+};
+
+/**
+* hash password with sha512.
+* @function
+* @param {string} password - List of required fields.
+* @param {string} salt - Data to be validated.
+*/
+function sha512(password, salt) {
+  var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+  hash.update(password);
+  var value = hash.digest('hex');
+  return {
+    salt:salt,
+    passwordHash:value
+  };
+};
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);

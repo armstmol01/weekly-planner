b43b70c7cbe2990b9bf8b52e079fef4d622fa003
armstmol01 <armstmol01@gmail.com>
2022-09-12 18:49:27 -0700

    tutorial pics

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -6,6 +6,16 @@ const About = () => {
   return (
     <div className="about__container">
       <Nav data='about' className='nav'></Nav>
+      <div>
+        <p>Enjoy this weekly planner to help your organize your week!</p>
+        <p>How to use:</p>
+        <ul>
+          <li>click the + icon to create a task</li>
+          <li>click the check icon to save task</li>
+          <li>click on task to cross it out/check it off once completed</li>
+          <li>double click task to delete it</li>
+        </ul>
+      </div>
     </div>
   )
 }

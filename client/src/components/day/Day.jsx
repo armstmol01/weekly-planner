89dbdbb6a0b57155ebb132ceba2be38bb557a314
armstmol01 <armstmol01@gmail.com>
2022-09-28 18:47:17 -0700

    scroll bottom on new task

--- client/src/components/day/Day.jsx
+++ client/src/components/day/Day.jsx
@@ -38,7 +38,6 @@ const Day = (props) => {
     setActive(false);
     // important in case that user adds an empty task by newTask is still value of previous
     newTask.current = "";
-    // document.querySelector(".new-task-input").value = "";
     let temp = taskState;
     temp.push({id: temp.length + 1, user_id: id, day: day, content: taskContent});
     setTaskState(temp);
@@ -46,12 +45,11 @@ const Day = (props) => {
   }
 
   const activeTask = () => {
-    newTask.current = ""; // make sure it's clear
-    // document.querySelector(".new-task-input").value = "";
     setActive(true);
-
     // important that it's delayed to make sure elem is rendered
-    setTimeout(() => {let elem = document.getElementById(scrollBottom); elem.scrollIntoView(false)}, 10);
+    setTimeout(() => {
+      let elem = document.getElementById(scrollBottom);
+      elem.scrollIntoView(false)}, 10);
   }
 
   return (
@@ -63,12 +61,8 @@ const Day = (props) => {
         <TaskList data={{userId: id, day: day, tasks: taskState}} />
         {active===true?<div id={scrollBottom} className='task new-task'>
           <div className='check__box'></div>
-          <input id='task-input' className='new-task-input' default="" spellCheck='false' onChange={(event) => {newTask.current = event.target.value}}></input>
+          <input id='task-input' default="" spellCheck='false' onChange={(event) => {newTask.current = event.target.value}}></input>
         </div>: ''}
-        {/* // <div id={scrollBottom} className={active===true?'task new-task':'hidden'}>
-        //   <div className='check__box'></div>
-        //   <input id='task-input' className='new-task-input' default="" spellCheck='false' onChange={(event) => {newTask.current = event.target.value}}></input>
-        // </div> */}
       </div>
     </section>
   )
